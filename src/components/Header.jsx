39b8785716d66063b5590d02ae7210f3cd831143
chinhcp <chinhcpdev@gmail.com>
2024-02-29 19:39:01 +0700

    update hover navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,17 +12,17 @@ function Header() {
   return (
     <div className="px-20 py-10 flex items-center justify-between border-b border-black">
       <div className="w-60">
-        {/* <img
-          src="https://lh3.google.com/u/0/d/1yecYnbygSK0KXLO714n2MGama5ecHQMH=w3024-h1646-iv1"
-          alt="logo-img"
-        /> */}
         <i className="text-2xl font-semibold font-sans-custom">
           Bachin Creative
         </i>
       </div>
       <div className="flex gap-8">
         {linkList.map((linkItem, index) => (
-          <NavLink key={index} to={linkItem.link} className="hover:underline">
+          <NavLink
+            key={index}
+            to={linkItem.link}
+            className="hover:underline hover:scale-105 transition-transform duration-300"
+          >
             {linkItem.title}
           </NavLink>
         ))}
